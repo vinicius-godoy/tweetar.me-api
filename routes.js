29998babec86e2652c7ce3c7925f7300594bc0f7
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-06-02 22:54:32 -0300

    feat: add get and post routes of tweet table

--- routes.js
+++ routes.js
@@ -1,7 +1,25 @@
 import Router from '@koa/router'
+import { PrismaClient } from '@prisma/client'
 
 export const router = new Router()
 
-router.get('/tweets', ctx => {
-  ctx.body = 'tweets'
+const prisma = new PrismaClient()
+
+router.get('/tweets', async ctx => {
+  const tweets = await prisma.tweet.findMany()
+
+  ctx.body = tweets
+})
+
+router.post('/tweets', async ctx => {
+  const tweet = {
+    userId: 'cl3xsd4jp0009ugntv8ed5hh4',
+    text: ctx.request.body.text,
+  }
+
+  const doc = await prisma.tweet.create({
+    data: tweet,
+  })
+
+  ctx.body = doc
 })
