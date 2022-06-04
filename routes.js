fe0b660277f673037acfbdc1cac5f41494d2e32e
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2022-06-03 22:39:12 -0300

    feat: add password hashing and jwt auth to db

--- routes.js
+++ routes.js
@@ -1,33 +1,107 @@
 import Router from '@koa/router'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+import { errorHandler } from './utils/errorHandler.js'
 
 export const router = new Router()
 
 const prisma = new PrismaClient()
 
-router.get('/tweets', async ctx => {
-  const tweets = ctx.query.id
-    ? await prisma.tweet.findUnique({
-      where: {
-        id: ctx.query.id,
+router.post('/signup', async ctx => {
+  const SALT_ROUNDS = 10
+  const hashedPassword = bcrypt.hashSync(
+    ctx.request.body.password,
+    SALT_ROUNDS
+  )
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: ctx.request.body.name,
+        username: ctx.request.body.username,
+        email: ctx.request.body.email,
+        password: hashedPassword,
       }
     })
-    : await prisma.tweet.findMany()
 
-  ctx.body = tweets
+    ctx.body = { ...user, password: undefined }
+  } catch (error) {
+    const [httpCode, payload] = errorHandler(error)
+    ctx.status = httpCode
+    ctx.body = payload
+  }
+})
+
+router.get('/login', async ctx => {
+  const [, token] = ctx.request.headers.authorization.split(' ')
+  const [email, password] = Buffer.from(token, 'base64').toString().split(':')
+
+  const user = await prisma.user.findUnique({
+    where: { email }
+  })
+  if (!user) {
+    ctx.status = 404
+    ctx.body = { status: 404, message: 'Not Found' }
+    return
+  }
+
+  const passwordMatch = bcrypt.compareSync(password, user.password)
+  if (!passwordMatch) {
+    ctx.status = 401
+    ctx.body = { status: 401, message: 'Unauthorized' }
+    return
+  }
+
+  const accessToken = jwt.sign({
+    sub: user.id
+  }, process.env.JWT_SECRET, { expiresIn: '24h' })
+
+  ctx.body = { ...user, accessToken, password: undefined }
+})
+
+router.get('/tweets', async ctx => {
+  try {
+    const [, token] = ctx.request.headers?.authorization?.split(' ') ?? []
+
+    jwt.verify(token, process.env.JWT_SECRET)
+
+    const tweets = ctx.query.id
+      ? await prisma.tweet.findUnique({
+        where: {
+          id: ctx.query.id,
+        }
+      })
+      : await prisma.tweet.findMany()
+
+    ctx.body = tweets
+  } catch (error) {
+    const [httpCode, payload] = errorHandler(error)
+    ctx.status = httpCode
+    ctx.body = payload
+  }
 })
 
 router.post('/tweets', async ctx => {
-  const tweet = {
-    userId: 'cl3xsd4jp0009ugntv8ed5hh4',
-    text: ctx.request.body.text,
+  try {
+    const [, token] = ctx.request.headers?.authorization?.split(' ') ?? []
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+    const tweet = await prisma.tweet.create({
+      data: {
+        userId: payload.sub,
+        text: ctx.request.body.text,
+      },
+    })
+
+    ctx.body = tweet
+  } catch (error) {
+    const [httpCode, payload] = errorHandler(error)
+    ctx.status = httpCode
+    ctx.body = payload
   }
-
-  const doc = await prisma.tweet.create({
-    data: tweet,
-  })
-
-  ctx.body = doc
 })
 
 router.delete('/tweets', async ctx => {
@@ -38,14 +112,15 @@ router.delete('/tweets', async ctx => {
   }
 
   try {
-    const doc = await prisma.tweet.delete({
+    const tweet = await prisma.tweet.delete({
       where: {
         id: ctx.query.id,
       }
     })
-    ctx.body = doc
+    ctx.body = tweet
   } catch (error) {
-    ctx.status = 404
-    ctx.body = { message: 'Not found', description: 'Tweet does not exist.' }
+    const [httpCode, payload] = errorHandler(error)
+    ctx.status = httpCode
+    ctx.body = payload
   }
 })
